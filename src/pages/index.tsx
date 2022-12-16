ee289917b0ba233d30ae8f3f89c24da9ea6ec361
Viperware Labs <111930874+viperware-labs@users.noreply.github.com>
2022-12-16 15:20:49 -0800

    Push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,16 +63,28 @@ function Page() {
     <div style={{
       height: `${size.height}px`
     }}
-    className={`h-[${size.height}px] overflow-clip `}>
+      className={`overflow-clip`}>
       {/* <div>
         {`${size.width} ${size.height}`}
       </div> */}
-      {seed == 1 ? <Image
+
+      {/* <Image
+        alt="Background"
+        src={`/images/${seed}bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        className="absolute w-full h-full"
+      /> */}
+      {/* {seed == 1 ? <Image
         alt="Background"
         src={`/images/1bg.png`}
         height={800}
         width={1500}
         quality={100}
+        style={{
+          hidden: `${(seed == 1)}`
+        }}
         className="absolute w-full h-full"
       /> : null}
       {seed == 2 ? <Image
@@ -138,7 +150,106 @@ function Page() {
         width={1500}
         quality={100}
         className="absolute w-full h-full"
-      /> : null}
+      /> : null} */}
+      <Image
+        alt="Background"
+        src={`/images/1bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 1 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/2bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 2 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/3bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 3 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/4bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 4 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/5bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 5 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/6bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 6 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/7bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 7 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/8bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 8 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
+      <Image
+        alt="Background"
+        src={`/images/9bg.png`}
+        height={800}
+        width={1500}
+        quality={100}
+        style={{
+          visibility: `${(seed == 9 ? "visible" : "hidden")}`
+        }}
+        className="absolute w-full h-full"
+      />
       <Head>
         <title>Free JoJo</title>
         <meta name="description" content="COLOR x Ownrshp" key="desc" />
@@ -155,7 +266,10 @@ function Page() {
 
           <>
             {/* Mobile */}
-            <div className="visible sm:invisible sm:h-0">
+            <div style={{
+              height: `${size.height}px`
+            }}
+              className="mobile overflow-clip">
               <div>
                 <div className="p-5 flex">
                   <div className="">
@@ -213,8 +327,8 @@ function Page() {
                       <Image
                         alt="We Are Jojo"
                         src={`/images/${seed}weare.png`}
-                        layout="fill"
-                        objectFit="cover"
+                        width={1000}
+                        height={1000}
                         className="h-[4rem] w-auto ml-14 mt-auto"
                       />
                     </div>
@@ -222,83 +336,86 @@ function Page() {
                 </div>
               </div>
             </div>
-          </> 
+          </>
 
           :
 
           <>
 
-          {/* Desktop */}
-          <div className={`h-[${size.height}px] overflow-hidden `}>
-            <div className="">
-              <div className="p-5 flex">
-                <div className="">
+            {/* Desktop */}
+            <div style={{
+              height: `${size.height}px`
+            }}
+              className="desktop overflow-clip">
+              <div className="">
+                <div className="p-5 flex">
+                  <div className="">
+                  </div>
+                  <div className="ml-auto z-10">
+                    <Connect />
+                  </div>
                 </div>
-                <div className="ml-auto z-10">
-                  <Connect />
-                </div>
-              </div>
-              <div className={styles.main}
-                style={{
-    
-                  fontFamily: 'Wave',
-                  fontWeight: '300'
-    
-                }}
-              >
-                <div className="text-white text-center w-full flex z-0">
-                  <div className="mx-auto center w-full">
-                    <Image
-                      alt="JoJo Words"
-                      src={`/images/${seed}free.png`}
-                      width={1000}
-                      height={1000}
-                      className="h-[30vw] min-h-[24rem] max-h-[30rem] w-auto -translate-y-[25vh] mx-auto z-10"
-                    />
-                    <div className="-translate-y-[50vh] w-fit mx-auto flex z-0">
+                <div className={styles.main}
+                  style={{
+
+                    fontFamily: 'Wave',
+                    fontWeight: '300'
+
+                  }}
+                >
+                  <div className="text-white text-center w-full flex z-0">
+                    <div className="mx-auto center w-full">
                       <Image
-                        alt="Click Here"
-                        src={ClickHere}
+                        alt="JoJo Words"
+                        src={`/images/${seed}free.png`}
                         width={1000}
                         height={1000}
-                        className="h-[20vh] mt-20 translate-y-[16vh] -translate-x-[4rem] absolute w-auto"
+                        className="h-[30vw] min-h-[24rem] max-h-[30rem] w-auto -translate-y-[25vh] mx-auto z-10"
                       />
-                      <div className="z-[100000] hover:cursor-pointer">
+                      <div className="-translate-y-[50vh] w-fit mx-auto flex z-0">
                         <Image
-                          alt="JoJo Image"
-                          src={`/images/${seed}jojo.png`}
+                          alt="Click Here"
+                          src={ClickHere}
                           width={1000}
                           height={1000}
-                          className="w-[50vw] min-w-[42rem] max-w-[50rem] mx-auto hover:cursor-pointer"
+                          className="h-[20vh] mt-20 translate-y-[16vh] -translate-x-[4rem] absolute w-auto"
                         />
+                        <div className="z-[100000] hover:cursor-pointer">
+                          <Image
+                            alt="JoJo Image"
+                            src={`/images/${seed}jojo.png`}
+                            width={1000}
+                            height={1000}
+                            className="w-[50vw] min-w-[42rem] max-w-[50rem] mx-auto hover:cursor-pointer"
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="absolute h-5/6 w-full flex" onClick={() => changeSeed()}>
-                    <div className="w-[50vw] min-w-[35rem] max-w-[40rem] h-[50vw] min-h-[35rem] max-h-[40rem] mx-auto p-1 hover:cursor-pointer hover:p-0 z-[100]"
-                    />
-                  </div>
-                  <div className="absolute h-5/6 w-full flex">
-                    <div className="mt-auto ml-auto mr-14 z-50">
-                      <MusicPlayer />
+                    <div className="absolute h-5/6 w-full flex" onClick={() => changeSeed()}>
+                      <div className="w-[50vw] min-w-[35rem] max-w-[40rem] h-[50vw] min-h-[35rem] max-h-[40rem] mx-auto p-1 hover:cursor-pointer hover:p-0 z-[100]"
+                      />
                     </div>
-                  </div>
-                  <div className="absolute h-5/6 w-full flex">
-                    <Image
-                      alt="We Are Jojo"
-                      src={`/images/${seed}weare.png`}
-                      width={1000}
-                      height={1000}
-                      className="h-[4rem] w-auto ml-14 mt-auto"
-                    />
+                    <div className="absolute h-5/6 w-full flex">
+                      <div className="mt-auto ml-auto mr-14 z-50">
+                        <MusicPlayer />
+                      </div>
+                    </div>
+                    <div className="absolute h-5/6 w-full flex">
+                      <Image
+                        alt="We Are Jojo"
+                        src={`/images/${seed}weare.png`}
+                        width={1000}
+                        height={1000}
+                        className="h-[4rem] w-auto ml-14 mt-auto"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
           </>
 
-    }
+      }
     </div>
   )
 }
