5a09d8d133e924a790e4ad0a23e0e5f18d4d8de8
Viperware Labs <111930874+viperware-labs@users.noreply.github.com>
2022-12-16 02:46:53 -0800

    Push

--- src/components/Minter.tsx
+++ src/components/Minter.tsx
@@ -11,13 +11,40 @@ import SignClient from "@walletconnect/sign-client";
 
 import Image from 'next/image';
 
-import GreenWords from '../../public/images/GreenWords.png'
-import GreenJoJo from '../../public/images/GreenJojo.png'
+import Free1 from '../../public/images/1free.png'
+import Jojo1 from '../../public/images/1jojo.png'
+import Weare1 from '../../public/images/1weare.png'
 
-import ClickHere from '../../public/images/ClickHere.png'
-import WeAreJojo from '../../public/images/WeAreJojo.png'
+import Free2 from '../../public/images/2free.png'
+import Jojo2 from '../../public/images/2jojo.png'
+import Weare2 from '../../public/images/2weare.png'
+
+import Free3 from '../../public/images/3free.png'
+import Jojo3 from '../../public/images/3jojo.png'
+import Weare3 from '../../public/images/3weare.png'
+
+import Free4 from '../../public/images/4free.png'
+import Jojo4 from '../../public/images/4jojo.png'
+import Weare4 from '../../public/images/4weare.png'
+
+import Free5 from '../../public/images/5free.png'
+import Jojo5 from '../../public/images/5jojo.png'
+import Weare5 from '../../public/images/5weare.png'
+
+import Free6 from '../../public/images/6free.png'
+import Jojo6 from '../../public/images/6jojo.png'
+import Weare6 from '../../public/images/6weare.png'
+
+import Free7 from '../../public/images/7free.png'
+import Jojo7 from '../../public/images/7jojo.png'
+import Weare7 from '../../public/images/7weare.png'
+
+import Free8 from '../../public/images/8free.png'
+import Jojo8 from '../../public/images/8jojo.png'
+import Weare8 from '../../public/images/8weare.png'
+
 import Play from '../../public/images/Play.png'
-
+import ClickHere from '../../public/images/ClickHere.png'
 
 import React from 'react'
 import ReactAudioPlayer from 'react-audio-player'
@@ -25,14 +52,16 @@ import { Connect } from './Button'
 
 import MusicPlayer from './Music'
 
+// @ts-ignore
+export function Minter({seed}) {
+	console.log(seed)
 
-export function Minter() {
 	// const { connectors, pendingConnector, connectAsync } = useConnect()
+
 	const { address, isConnected } = useAccount();
 	const { chain, chains } = useNetwork();
 	const { data: ensName } = useEnsName({ address });
 
-	const [seed, setSeed] = useState(0);
 	const [difficulty, setDifficulty] = useState(0);
 	const [seedMatches, setSeedMatches] = useState(false);
 	const [miningEnabled, setMiningEnabled] = useState(false);
@@ -43,7 +72,7 @@ export function Minter() {
 				<div className="mx-auto center w-full">
 					<Image
 						alt="JoJo Words"
-						src={GreenWords}
+						src={Free8}
 						className="h-[30vw] min-h-[24rem] max-h-[30rem] w-auto -translate-y-[25vh] mx-auto z-10"
 					/>
 					<div className="-translate-y-[50vh] w-fit mx-auto flex z-0">
@@ -55,7 +84,7 @@ export function Minter() {
 						<div className="z-[100000] hover:cursor-pointer">
 							<Image
 								alt="JoJo Image"
-								src={GreenJoJo}
+								src={Jojo8}
 								className="w-[50vw] min-w-[42rem] max-w-[50rem] mx-auto hover:cursor-pointer"
 							/>
 						</div>
@@ -63,7 +92,7 @@ export function Minter() {
 				</div>
 				<div className="absolute h-5/6 w-full flex">
 					<div className="w-[50vw] min-w-[35rem] max-w-[40rem] h-[50vw] min-h-[35rem] max-h-[40rem] mx-auto p-1 hover:cursor-pointer hover:p-0 z-50"
-						// onClick={something}
+						// onClick={changeSeed}
 						/>
 				</div>
 				<div className="absolute h-5/6 w-full flex">
@@ -74,7 +103,7 @@ export function Minter() {
 				<div className="absolute h-5/6 w-full flex">
 					<Image
 						alt="We Are Jojo"
-						src={WeAreJojo}
+						src={Weare8}
 						className="h-[4rem] w-auto ml-14 mt-auto"
 					/>
 				</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,53 @@ import dynamic from "next/dynamic"
 import styles from '../../styles/Home.module.css';
 import { Minter } from '../components'
 
-import Image from 'next/image';
 import JoJoList from '../../public/images/JoJoList.png'
+
+import { createClient, useEnsName, useNetwork } from 'wagmi'
+import keccupABI from '../abi.json';
+import { ethers } from "ethers";
+import { keccak256, parseEther } from 'ethers/lib/utils';
+import { useContractReads } from 'wagmi';
+import { useState } from 'react';
+
+import wagmi from 'wagmi'
+import SignClient from "@walletconnect/sign-client";
+
+import Image from 'next/image';
+
+import Play from '../../public/images/Play.png'
+import ClickHere from '../../public/images/ClickHere.png'
 
 import React from 'react'
 import ReactAudioPlayer from 'react-audio-player'
 import { Connect } from '../components/Button'
 
+import MusicPlayer from '../components/Music'
+
+
 function Page() {
-  const { isConnected } = useAccount()
+	const [seed, setSeed] = useState(Math.floor(Math.random() * 8) + 1);
 	const { connect, connectors, error, isLoading, pendingConnector } = useConnect();
+
+  const changeSeed = () => {
+    setSeed(Math.floor(Math.random() * 8) + 1);
+  }
+
+	// const { connectors, pendingConnector, connectAsync } = useConnect()
+
+	const { address, isConnected } = useAccount();
+	const { chain, chains } = useNetwork();
+	const { data: ensName } = useEnsName({ address });
+
+	const [difficulty, setDifficulty] = useState(0);
+	const [seedMatches, setSeedMatches] = useState(false);
+	const [miningEnabled, setMiningEnabled] = useState(false);
+
   return (
     <div className="h-screen overflow-hidden">
-      <div className={styles.background}>
+      <div className="" style={{
+        backgroundImage: `url("/images/${seed}bg.png")`,
+      }}>
         <Head>
           <title>Free JoJo</title>
           <meta name="description" content="COLOR x Ownrshp" key="desc" />
@@ -44,7 +78,45 @@ function Page() {
 
           }}
         >
-          <Minter />
+          <div className="text-white text-center w-full flex z-0">
+            <div className="mx-auto center w-full">
+              <img
+                alt="JoJo Words"
+                src={`/images/${seed}free.png`}
+                className="h-[30vw] min-h-[24rem] max-h-[30rem] w-auto -translate-y-[25vh] mx-auto z-10"
+              />
+              <div className="-translate-y-[50vh] w-fit mx-auto flex z-0">
+                <Image
+                  alt="Click Here"
+                  src={ClickHere}
+                  className="h-[20vh] mt-20 translate-y-[16vh] -translate-x-[4rem] absolute w-auto"
+                />
+                <div className="z-[100000] hover:cursor-pointer">
+                  <img
+                    alt="JoJo Image"
+                    src={`/images/${seed}jojo.png`}
+                    className="w-[50vw] min-w-[42rem] max-w-[50rem] mx-auto hover:cursor-pointer"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="absolute h-5/6 w-full flex" onClick={() => changeSeed()}>
+              <div className="w-[50vw] min-w-[35rem] max-w-[40rem] h-[50vw] min-h-[35rem] max-h-[40rem] mx-auto p-1 hover:cursor-pointer hover:p-0 z-[100]"
+                />
+            </div>
+            <div className="absolute h-5/6 w-full flex">
+              <div className="mt-auto ml-auto mr-14 z-50">
+                <MusicPlayer />
+              </div>
+            </div>
+            <div className="absolute h-5/6 w-full flex">
+              <img
+                alt="We Are Jojo"
+                src={`/images/${seed}weare.png`}
+                className="h-[4rem] w-auto ml-14 mt-auto"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
