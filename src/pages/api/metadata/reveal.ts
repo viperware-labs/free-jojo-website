5454ff3e99487be33e0dcb8a05e968d98a0f3065
Viperware Labs <111930874+viperware-labs@users.noreply.github.com>
2023-01-27 00:19:27 -0800

    Update reveal.ts

--- src/pages/api/metadata/reveal.ts
+++ src/pages/api/metadata/reveal.ts
@@ -23,79 +23,81 @@ export default async function handler(req, res) {
     switch (method) {
         case 'POST':
             try {
-                try {
-                    console.log("Reveal action start");
-
-                    // Signature to ensure user is making tx based on his wallet
-                    const signature = req.body.data.signature;
-                    const message = req.body.data.message;
-                    const siweMessage = new SiweMessage(message);
-
-                    await siweMessage.validate(signature)
-
-                    // Valid signature yields below
-
-                    const userAddress = siweMessage.address;
-                    const statement = siweMessage.statement;
-
-                    console.log("Valid")
-
-                    console.log(userAddress, statement)
-
-                    var ownedTokens: any[] = []
-
+                if (false) {
                     try {
+                        console.log("Reveal action start");
 
-                        await contractJOJO.functions.tokensOfOwner(userAddress).then((result) => {
-                            // @ts-ignore
-                            ownedTokens = result[0].map((bigNum) => formatEther(bigNum as number) * (10 ** 18))
-                            console.log(ownedTokens)
-                        });
+                        // Signature to ensure user is making tx based on his wallet
+                        const signature = req.body.data.signature;
+                        const message = req.body.data.message;
+                        const siweMessage = new SiweMessage(message);
 
-                    } catch (e) {
+                        await siweMessage.validate(signature)
 
-                        console.log("Error fetching tokens!", e)
-                        return res.status(400).json({ success: false, error: "Error fetching tokens!" });
-               
-                    }
+                        // Valid signature yields below
 
-                    await dbConnect()
-                    const reveals = statement?.split(",").map((num) => parseInt(num))
+                        const userAddress = siweMessage.address;
+                        const statement = siweMessage.statement;
 
-                    await reveals?.forEach(async (index) => {
+                        console.log("Valid")
+
+                        console.log(userAddress, statement)
+
+                        var ownedTokens: any[] = []
+
                         try {
 
-                            if (ownedTokens.includes(index)) {
-                                const nftToken = await NFT.findOne({
-                                    id: index
-                                });
-
-                                if (nftToken) {
-                                    nftToken.revealed = true;
-                                    nftToken.save();
-                                    console.log(`Successfully revealed #${index}`)
-                                    // return res.status(400).json({ success: true })
-                                } else {
-                                    const newNFT = await NFT.create({
-                                        id: index,
-                                        revealed: true,
-                                    })
-                                    console.log(`Successfully revealed #${index}`)
-                                    // res.status(201).json({ success: true, data: newNFT })
-                                }
-                            } else {
-                                console.log(`Error: User does not own #${index}`)
-                            }
+                            await contractJOJO.functions.tokensOfOwner(userAddress).then((result) => {
+                                // @ts-ignore
+                                ownedTokens = result[0].map((bigNum) => formatEther(bigNum as number) * (10 ** 18))
+                                console.log(ownedTokens)
+                            });
 
                         } catch (e) {
-                            console.log(`Could not reveal #${index}`)
+
+                            console.log("Error fetching tokens!", e)
+                            return res.status(400).json({ success: false, error: "Error fetching tokens!" });
+
                         }
-                    })
-                    
-                    return res.status(201).json({ success: true, data: reveals });
-                } catch (e) {
-                    console.log(e)
-                    return res.status(400).json({ success: false, error: "Signature failed, please verify you are using the correct wallet" });
+
+                        await dbConnect()
+                        const reveals = statement?.split(",").map((num) => parseInt(num))
+
+                        await reveals?.forEach(async (index) => {
+                            try {
+
+                                if (ownedTokens.includes(index)) {
+                                    const nftToken = await NFT.findOne({
+                                        id: index
+                                    });
+
+                                    if (nftToken) {
+                                        nftToken.revealed = true;
+                                        nftToken.save();
+                                        console.log(`Successfully revealed #${index}`)
+                                        // return res.status(400).json({ success: true })
+                                    } else {
+                                        const newNFT = await NFT.create({
+                                            id: index,
+                                            revealed: true,
+                                        })
+                                        console.log(`Successfully revealed #${index}`)
+                                        // res.status(201).json({ success: true, data: newNFT })
+                                    }
+                                } else {
+                                    console.log(`Error: User does not own #${index}`)
+                                }
+
+                            } catch (e) {
+                                console.log(`Could not reveal #${index}`)
+                            }
+                        })
+
+                        return res.status(201).json({ success: true, data: reveals });
+                    } catch (e) {
+                        console.log(e)
+                        return res.status(400).json({ success: false, error: "Signature failed, please verify you are using the correct wallet" });
+                    }
                 }
             } catch (error) {
                 res.status(400).json({ success: false })
