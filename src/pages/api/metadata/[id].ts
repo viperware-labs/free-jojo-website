bbff8708a3a5679c9439091334446714e40b50e4
Viperware Labs <111930874+viperware-labs@users.noreply.github.com>
2023-02-04 15:09:28 -0800

    Update [id].ts

--- src/pages/api/metadata/[id].ts
+++ src/pages/api/metadata/[id].ts
@@ -18,7 +18,7 @@ const getKey = (request: express.Request, response: express.Response) => {
 
 const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 5, // limit each IP to 100 requests per windowMs
     message: 'Too many requests, please try again later',
     keyGenerator: (request, response) => (getKey(request, response) as string)
 });
