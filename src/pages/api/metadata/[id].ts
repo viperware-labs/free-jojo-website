682f65ca48e85af3c75082c817c706af8a48ae11
Viperware Labs <111930874+viperware-labs@users.noreply.github.com>
2023-02-04 09:53:07 -0800

    Update [id].ts

--- src/pages/api/metadata/[id].ts
+++ src/pages/api/metadata/[id].ts
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
         method
     } = req
     const MAX_ID = 10000
+    await dbConnect()
     switch (method) {
         case 'GET':
             try {
@@ -25,7 +26,6 @@ export default async function handler(req, res) {
                 } else if (id < 1 || id > MAX_ID) {
                     res.status(400).json({ success: false, error: "Out of bounds" })
                 } else {
-                    await dbConnect()
                     console.log("findone", id);
                     const nftToken = await NFT.findOne({
                         id: id,
