337b7c9289f6f96ab326958575385396f4f9f511
Viperware Labs <111930874+viperware-labs@users.noreply.github.com>
2023-02-04 15:01:17 -0800

    Update [id].ts

--- src/pages/api/metadata/[id].ts
+++ src/pages/api/metadata/[id].ts
@@ -9,6 +9,7 @@ const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
     message: 'Too many requests, please try again later',
+    keyGenerator: (request, response) => request.ip
 });
 
 // REVEALED
